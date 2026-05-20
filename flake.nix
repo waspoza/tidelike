451071bf768d9feb58387b359f57966a1055e489
waspoza <waspoza@gmail.com>
2026-05-20 15:39:51 +0200

    flake.nix added

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Search for text inside emails";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "tidelike";
+          version = "0.1.0";
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          doCheck = false; # speed thing up by avoiding second build
+
+          meta = with pkgs.lib; {
+            description = "Change file path to shortest unique form, like tide does";
+            mainProgram = "tidelike";
+            license = licenses.bsd3;
+            platforms = platforms.linux;
+          };
+        };
+      }
+    );
+}
